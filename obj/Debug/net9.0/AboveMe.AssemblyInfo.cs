09515eb80939ac23ce19232503947403558c88a0
Mark Owens <mowens@orionhealthcare.onmicrosoft.com>
2025-09-14 17:15:07 -0400

    nuget package updates

--- obj/Debug/net9.0/AboveMe.AssemblyInfo.cs
+++ obj/Debug/net9.0/AboveMe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AboveMe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3e2df546f6fd3d98ae35e66402d8cd3d545a0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae3fe94fa6114e8eacbc523502248b9eb3136a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AboveMe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AboveMe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
