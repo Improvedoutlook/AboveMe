5751b2426c3aaf0f749ecf9a0f5d7165cbebe5a0
Mark Owens <mowens@orionhealthcare.onmicrosoft.com>
2026-01-11 12:07:54 -0500

    updated .yml file as Actions was not working properly Added api key back in place of DEMO_KEY variables and removed debug logging

--- wwwroot/getUserLocation.js
+++ wwwroot/getUserLocation.js
@@ -1,23 +1,17 @@
 // JS interop for geolocation
 window.getUserLocation = function () {
-    console.log("getUserLocation called");
     return new Promise(function (resolve, reject) {
         if (!navigator.geolocation) {
-            console.log("Geolocation not supported by browser");
             resolve(null);
             return;
         }
-        console.log("Requesting geolocation permission...");
         navigator.geolocation.getCurrentPosition(function (position) {
-            console.log("Geolocation success:", position.coords.latitude, position.coords.longitude);
             var result = {
                 Latitude: position.coords.latitude,
                 Longitude: position.coords.longitude
             };
-            console.log("Returning result:", JSON.stringify(result));
             resolve(result);
         }, function (error) {
-            console.log("Geolocation error:", error.code, error.message);
             resolve(null);
         }, {
             enableHighAccuracy: true,
